bd625a64d2bb1ce54e91e0673ecb01ebc7415f6d
mariliasantos21m <mariliasantosnas21@gmail.com>
2025-05-06 23:07:58 -0300

    Initial commit

---
+++ src/main/java/com/tcc/api_ticket_sales/ApiTicketSalesApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.api_ticket_sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTicketSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTicketSalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcc/api_ticket_sales/ApiTicketSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.api_ticket_sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTicketSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
