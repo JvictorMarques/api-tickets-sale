63679abd8f53d07662e15f5109929f805368750b
Marilia Santos <marilia@novetech.com.br>
2025-10-06 22:31:11 -0300

    test: Adiciona testes unitários para os métodos da classe CheckDate

--- app/src/main/java/com/tcc/api_ticket_sales/domain/utils/CheckDate.java
+++ app/src/main/java/com/tcc/api_ticket_sales/domain/utils/CheckDate.java
@@ -1,6 +1,7 @@
 package com.tcc.api_ticket_sales.domain.utils;
 
 import com.tcc.api_ticket_sales.domain.exception.DateInitialGreaterThanDateFinalException;
+import com.tcc.api_ticket_sales.domain.exception.DateInvalidException;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -16,12 +17,20 @@ public class CheckDate {
 //    }
 
     public static void checkDateInitialGreaterThanDateFinal(LocalDate dateInitial, LocalDate dateFinal) {
+        if(dateInitial == null || dateFinal == null){
+            throw new DateInvalidException();
+        }
+
         if(dateInitial.isAfter(dateFinal)){
             throw new DateInitialGreaterThanDateFinalException();
         }
     }
 
     public static void checkDateInitialGreaterThanDateFinal(LocalDateTime dateInitial, LocalDateTime dateFinal) {
+        if(dateInitial == null || dateFinal == null){
+            throw new DateInvalidException();
+        }
+
         if(dateInitial.isAfter(dateFinal)){
             throw new DateInitialGreaterThanDateFinalException();
         }
---
+++ app/src/test/java/com/tcc/api_ticket_sales/domain/utils/CheckDateTest.java
@@ -0,0 +1,132 @@
+package com.tcc.api_ticket_sales.domain.utils;
+
+import com.tcc.api_ticket_sales.domain.exception.DateInitialGreaterThanDateFinalException;
+import com.tcc.api_ticket_sales.domain.exception.DateInvalidException;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CheckDateTest {
+
+    @Test
+    public void checkDateInitialGreaterThanDateFinal_shouldNotThrowException_whenInitialBeforeFinal(){
+        LocalDate start = LocalDate.now();
+        LocalDate end = LocalDate.now().plusDays(1);
+
+        assertDoesNotThrow(() ->
+                CheckDate.checkDateInitialGreaterThanDateFinal(start, end)
+        );
+    }
+
+    @Test
+    public void checkDateInitialGreaterThanDateFinal_shouldNotThrowException_whenInitialEqualFinal(){
+        LocalDate start = LocalDate.now();
+        LocalDate end = LocalDate.now();
+
+        assertDoesNotThrow(() ->
+                CheckDate.checkDateInitialGreaterThanDateFinal(start, end)
+        );
+    }
+
+    @Test
+    public void checkDateInitialGreaterThanDateFinal_shouldThrowException_whenInitialAfterFinal(){
+        LocalDate start = LocalDate.now().plusDays(1);
+        LocalDate end = LocalDate.now();
+
+        assertThrows(DateInitialGreaterThanDateFinalException.class, () ->
+                CheckDate.checkDateInitialGreaterThanDateFinal(start, end)
+        );
+    }
+
+    @Test
+    public void checkDateInitialGreaterThanDateFinal_shouldThrowException_whenInitialNull(){
+        LocalDate start = null;
+        LocalDate end = LocalDate.now();
+
+        assertThrows(DateInvalidException.class, () ->
+                CheckDate.checkDateInitialGreaterThanDateFinal(start, end)
+        );
+    }
+
+    @Test
+    public void checkDateInitialGreaterThanDateFinal_shouldThrowException_whenFinalNull(){
+        LocalDate start = LocalDate.now();
+        LocalDate end = null;
+
+        assertThrows(DateInvalidException.class, () ->
+                CheckDate.checkDateInitialGreaterThanDateFinal(start, end)
+        );
+    }
+
+    @Test
+    public void checkDateInitialGreaterThanDateFinal_shouldThrowException_whenInitialFinalNull(){
+        LocalDate start = null;
+        LocalDate end = null;
+
+        assertThrows(DateInvalidException.class, () ->
+                CheckDate.checkDateInitialGreaterThanDateFinal(start, end)
+        );
+    }
+
+    @Test
+    public void checkDateInitialGreaterThanDateFinal_LocalDateTime_shouldNotThrowException_whenInitialBeforeFinal(){
+        LocalDateTime start = LocalDateTime.now();
+        LocalDateTime end = LocalDateTime.now().plusDays(1);
+
+        assertDoesNotThrow(() ->
+                CheckDate.checkDateInitialGreaterThanDateFinal(start, end)
+        );
+    }
+
+    @Test
+    public void checkDateInitialGreaterThanDateFinal_LocalDateTime_shouldNotThrowException_whenInitialEqualFinal(){
+        LocalDateTime start = LocalDateTime.now();
+        LocalDateTime end = LocalDateTime.now();
+
+        assertDoesNotThrow(() ->
+                CheckDate.checkDateInitialGreaterThanDateFinal(start, end)
+        );
+    }
+
+    @Test
+    public void checkDateInitialGreaterThanDateFinal_LocalDateTime_shouldThrowException_whenInitialAfterFinal(){
+        LocalDateTime start = LocalDateTime.now().plusDays(1);
+        LocalDateTime end = LocalDateTime.now();
+
+        assertThrows(DateInitialGreaterThanDateFinalException.class, () ->
+                CheckDate.checkDateInitialGreaterThanDateFinal(start, end)
+        );
+    }
+
+    @Test
+    public void checkDateInitialGreaterThanDateFinal_LocalDateTime_shouldThrowException_whenInitialNull(){
+        LocalDateTime start = null;
+        LocalDateTime end = LocalDateTime.now();
+
+        assertThrows(DateInvalidException.class, () ->
+                CheckDate.checkDateInitialGreaterThanDateFinal(start, end)
+        );
+    }
+
+    @Test
+    public void checkDateInitialGreaterThanDateFinal_LocalDateTime_shouldThrowException_whenFinalNull(){
+        LocalDateTime start = LocalDateTime.now();
+        LocalDateTime end = null;
+
+        assertThrows(DateInvalidException.class, () ->
+                CheckDate.checkDateInitialGreaterThanDateFinal(start, end)
+        );
+    }
+
+    @Test
+    public void checkDateInitialGreaterThanDateFinal_LocalDateTime_shouldThrowException_whenInitialFinalNull(){
+        LocalDateTime start = null;
+        LocalDateTime end = null;
+
+        assertThrows(DateInvalidException.class, () ->
+                CheckDate.checkDateInitialGreaterThanDateFinal(start, end)
+        );
+    }
+}
