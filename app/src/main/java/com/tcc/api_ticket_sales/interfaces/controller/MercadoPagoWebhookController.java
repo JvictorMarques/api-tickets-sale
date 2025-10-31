5cbadc47e818ec11c03cb01400e4dc020f45ccef
Marilia Santos <marilia@novetech.com.br>
2025-10-30 22:41:00 -0300

    feat: Adiciona o swagger para o endpoint de compra e recebimento de notificação do mercado pago

--- app/src/main/java/com/tcc/api_ticket_sales/interfaces/controller/MercadoPagoWebhookController.java
+++ app/src/main/java/com/tcc/api_ticket_sales/interfaces/controller/MercadoPagoWebhookController.java
@@ -2,6 +2,8 @@ package com.tcc.api_ticket_sales.interfaces.controller;
 
 import com.tcc.api_ticket_sales.application.service.webhook.MercadoPagoWebhookService;
 import com.tcc.api_ticket_sales.application.dto.webhook.mercadopago.MercadoPagoWebhoookRequestDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,10 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/webhooks/mercadopago")
 @RequiredArgsConstructor
+@Tag(name = "Webhooks - MercadoPago")
 public class MercadoPagoWebhookController {
 
     private final MercadoPagoWebhookService mercadoPagoWebhookService;
 
+    @Operation(
+            summary = "Receber notificações do MercadoPago"
+    )
     @PostMapping
     public ResponseEntity<Void>  receiveNotification(@RequestBody @Valid MercadoPagoWebhoookRequestDTO requestDTO){
         mercadoPagoWebhookService.processNotification(requestDTO);
