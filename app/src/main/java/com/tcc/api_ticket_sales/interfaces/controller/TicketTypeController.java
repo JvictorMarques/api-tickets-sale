65e06c1220fa15bdfa4283eab95c36737d2af107
Marilia Santos <marilia@novetech.com.br>
2025-11-03 20:54:29 -0300

    feat: Adiciona swagger no endpoint para edição do tipo de ingresso

--- app/src/main/java/com/tcc/api_ticket_sales/interfaces/controller/TicketTypeController.java
+++ app/src/main/java/com/tcc/api_ticket_sales/interfaces/controller/TicketTypeController.java
@@ -3,6 +3,13 @@ package com.tcc.api_ticket_sales.interfaces.controller;
 import com.tcc.api_ticket_sales.application.dto.ticket_type.TicketTypeResponseDTO;
 import com.tcc.api_ticket_sales.application.dto.ticket_type.TicketTypeUpdateRequestDTO;
 import com.tcc.api_ticket_sales.application.service.ticket_type.TicketTypeService;
+import com.tcc.api_ticket_sales.interfaces.controller.exception.RestExceptionMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +30,150 @@ public class TicketTypeController {
 
     private final TicketTypeService ticketTypeService;
 
+    @Operation(
+            summary = "Editar tipo de ingresso"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tipo de ingresso editado com sucesso"),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Formato de requisição inválido e validações dos campos",
+                    content = @Content(
+                            schema = @Schema(implementation = RestExceptionMessage.class),
+                            examples= {
+                                    @ExampleObject(
+                                            summary = "Validação de campo",
+                                            value = """
+                                            {
+                                                "message": "Erro de validação",
+                                                "status": 400,
+                                                "timeStamp": "2025-10-13T18:00:00",
+                                                "errors": [
+                                                    "capacity": "A capacidade deve ser um número positivo.",
+                                                ]
+                                            }
+                                            """
+                                    )
+                            }
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Recursos não encontrados",
+                    content = @Content(
+                            schema = @Schema(implementation = RestExceptionMessage.class),
+                            examples = {
+                                    @ExampleObject(
+                                            summary = "Tipo de ingresso não encontrado",
+                                            value = """
+                                            {
+                                                "message": "Tipo de ingresso [id] não encontrado.",
+                                                "status": 404,
+                                                "timeStamp": "2025-10-13T18:00:00",
+                                                "errors": [
+                                                    "Tipo de ingresso [id] não encontrado."
+                                                ]
+                                            }
+                                            """
+                                    ),
+                            }
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "Conflitos",
+                    content = @Content(
+                            schema = @Schema(implementation = RestExceptionMessage.class),
+                            examples = {
+                                    @ExampleObject(
+                                            name = "Capacidade menor que ingressos vendidos",
+                                            summary = "Capacidade menor que ingressos vendidos",
+                                            value = """
+                                            {
+                                                "message": "Quantidade inválida para atualização: existem [quantidade] ingressos já vendidos.",
+                                                "status": 409,
+                                                "timeStamp": "2025-10-13T18:00:00",
+                                                "errors": [
+                                                    "Quantidade inválida para atualização: existem [quantidade] ingressos já vendidos."
+                                                ]
+                                            }
+                                            """
+                                    ),
+                                    @ExampleObject(
+                                            name="Capacidade excede a capacidade do evento",
+                                            summary = "Capacidade excede a capacidade do evento",
+                                            value = """
+                                            {
+                                                "message": "Capacidade insuficiente: apenas 3 vagas restantes no evento para novos tipos de ingressos.",
+                                                "status": 409,
+                                                "timeStamp": "2025-10-13T18:00:00",
+                                                "errors": [
+                                                    "Capacidade insuficiente: apenas 3 vagas restantes no evento para novos tipos de ingressos."
+                                                ]
+                                            }
+                                            """
+                                    ),
+                                    @ExampleObject(
+                                            name = "A data de venda do tipo de ingresso não pode ocorrer após a data do evento",
+                                            summary = "A data de venda do tipo de ingresso não pode ocorrer após a data do evento",
+                                            value = """
+                                            {
+                                                "message": "A data de venda dos ingressos não pode ser após o evento.",
+                                                "status": 409,
+                                                "timeStamp": "2025-10-13T18:00:00",
+                                                "errors": [
+                                                    "A data de venda dos ingressos não pode ser após o evento."
+                                                ]
+                                            }
+                                            """
+                                    ),
+                                    @ExampleObject(
+                                            name = "O evento já possui um tipo de ingresso com as mesmas características",
+                                            summary = "O evento já possui um tipo de ingresso com as mesmas características",
+                                            value = """
+                                            {
+                                                "message": "Tipo de ingresso duplicado: este evento já possui o tipo de ingresso informado.",
+                                                "status": 409,
+                                                "timeStamp": "2025-10-13T18:00:00",
+                                                "errors": [
+                                                    "Tipo de ingresso duplicado: este evento já possui o tipo de ingresso informado."
+                                                ]
+                                            }
+                                            """
+                                    ),
+                                    @ExampleObject(
+                                            name = "Tipo de ingresso encerrado",
+                                            summary = "Tipo de ingresso encerrado",
+                                            value = """
+                                            {
+                                                "message": "Tipo de ingresso encerrado.",
+                                                "status": 409,
+                                                "timeStamp": "2025-10-13T18:00:00",
+                                                "errors": [
+                                                    "Tipo de ingresso encerrado."
+                                                ]
+                                            }
+                                            """
+                                    ),
+                                    @ExampleObject(
+                                            name = "Evento encerrado",
+                                            summary = "Evento encerrado",
+                                            value = """
+                                            {
+                                                "message": "Evento encerrado.",
+                                                "status": 409,
+                                                "timeStamp": "2025-10-13T18:00:00",
+                                                "errors": [
+                                                    "Evento encerrado."
+                                                ]
+                                            }
+                                            """
+                                    )
+                            }
+
+                    )
+            ),
+    })
     @PatchMapping("/{ticketTypeId}")
     public ResponseEntity<TicketTypeResponseDTO> update(@PathVariable UUID ticketTypeId, @Valid @RequestBody TicketTypeUpdateRequestDTO dto) {
         TicketTypeResponseDTO responseDTO = ticketTypeService.update(ticketTypeId, dto);
